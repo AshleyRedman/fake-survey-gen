e41253eec093c8d32b5854794737d48ffb1258cd
Ashley Redman <ash.redman@outlook.com>
2023-04-07 21:48:57 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import fs from 'fs';
 import { generateMock } from '@anatine/zod-mock';
 import { SurveyJsonSchema } from './schema';
 
-const mock = generateMock(SurveyJsonSchema);
+const mock = generateMock(SurveyJsonSchema, {
+    stringMap: {
+        title: () => `This is the name of the survey, hence testable`
+    }
+});
 
 fs.writeFile('example.json', JSON.stringify(mock), {}, () => {});
